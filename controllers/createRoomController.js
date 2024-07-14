98434e5fe08fd5b3f81ad54e79d3dd61b38ca080
Tim Ling <lingt@usc.edu>
2024-07-14 15:24:06 -0400

    Fixed async issue and implemented generating unique room ids and saving it to MongoDB using Mongoose ORM

--- controllers/createRoomController.js
+++ controllers/createRoomController.js
@@ -1,54 +1,42 @@
 const { v4: uuidv4 } = require('uuid');
 var Room = require('../models/room');
 
-exports.createPrivateRoom = function (){
+exports.createPrivateRoom = async function (req, res, next){
 
+	console.log("reach122");
+	let isUnique = false;
+	let generatedId="";
 
-	return async function(req,res,next)
+	while(!isUnique)
 	{
-		console.log("reach122");
-		let isUnique = false;
-		let generatedId="";
+		generatedId = uuidv4().toLowerCase();
+		console.log(generatedId);
 
-		console.log("reach12");
-
-		while(!isUnique)
-		{
-			generatedId = uniqid().toLowerCase();
-			console.log(generatedId);
-
-			await new Promise((resolve,reject)=>{
-				Room.find({roomId:generatedId}).exec(function(err,result){
-					if(err){return next(err);}
-					if(result.length === 0)
-					{
-						isUnique = true;
-						resolve(true);
-					}
-				});
+		await new Promise((resolve,reject)=>{
+			Room.find({roomId:generatedId}).exec().then(async docs => {
+			    if (docs.length > 0) {
+			      console.log('Documents found:', docs);
+			    }
+			    else {
+			      	console.log('No documents found');
+			      	try {
+			      		const room = await new Room({
+							connections:[{userId: req.body.userId, socketId:req.body.socketId}],
+							roomId:generatedId,
+							isPublic:false,
+							//roomData: roomDataId,
+						}).save();
+						console.log("Room:" + generatedId + " saved.");
+			      	}
+			      	catch (err) {
+			      		console.error('Error saving room:', err);
+			      	}
+			      	
+			    }
+			  })
+			  .catch(err => {
+			    console.error('Error occurred:', err);
 			});
-		}
-
-		const room = new Room({
-					connections:[{userId: req.body.userId, socketId:req.body.socketId}],
-					roomId:generatedId,
-					isPublic:false,
-					//roomData: roomDataId,
-					
-				}).save(err=>{
-					if(err){
-						return next(err);
-					} 
-		
-				return res.json({
-					createdRoom: true,
-					roomId:generatedId,
-				});
-
-		
 		});
-		
-
 	}
-
 };
